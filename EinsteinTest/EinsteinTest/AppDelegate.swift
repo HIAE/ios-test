7a4c8a572db3233264019e75cc5b7dc5a9389100
Pedro Lopes <>
2020-02-18 00:17:43 -0300

    reorder project structure

--- EinsteinTest/EinsteinTest/AppDelegate.swift
+++ EinsteinTest/EinsteinTest/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  EinsteinTest
 //
-//  Created by Pedro Lopes on 16/02/20.
+//  Created by Pedro Lopes on 18/02/20.
 //  Copyright © 2020 Pedro Lopes. All rights reserved.
 //
 
