2fd8d6027d55e1b7fa7e212acef467c6f8f3b213
Henrique Melo <henry.melo@yahoo.com>
2020-02-20 09:08:04 -0300

    strutura inicial do Projeto SoRRINDO

---
+++ SoRRINDO/SoRRINDO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoRRINDO
+//
+//  Created by Henrique Melo on 20/02/20.
+//  Copyright © 2020 Henrique Melo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Iniciando SoRRINDO... Estarei adicionando os componentes UI... conforme SpecSoRRINDO.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
