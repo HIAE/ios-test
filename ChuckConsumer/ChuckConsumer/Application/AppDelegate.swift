ab7dc788a60dcec2be0ab91048170bad32253eb2
dede.exe <dede.exe@gmail.com>
2020-03-03 22:52:40 -0300

    add: AppMain entity to control entrance app flow

--- ChuckConsumer/ChuckConsumer/Application/AppDelegate.swift
+++ ChuckConsumer/ChuckConsumer/Application/AppDelegate.swift
@@ -1,21 +1,18 @@
-//
-//  AppDelegate.swift
-//  ChuckConsumer
-//
-//  Created by dede.exe on 03/03/20.
-//  Copyright © 2020 dede. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    var appMain: AppMain!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        appMain = AppMain(using: window)
+        
+        return appMain.run()
     }
+    
 }
 
---
+++ ChuckConsumer/ChuckConsumer/Application/AppMain.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class AppMain {
+    var window: UIWindow?
+    
+    init(using window: UIWindow?) {
+        self.window = window
+    }
+    
+    func run() -> Bool {
+        guard let window = self.window else {
+            return false
+        }
+        
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = UIColor.gray
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        return true
+    }
+}
