056f5647b973bc733a00f9c1e3d91f88dc62a6a4
Vinicius Custodio <vinicustod@hotmail.com>
2020-07-17 20:45:31 -0300

    Git Ignore

---
+++ HIAEChallenge/HIAEChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HIAEChallenge
+//
+//  Created by Vinicius Custodio on 17/07/20.
+//  Copyright © 2020 Vinicius Custodio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
