b24df0b8a8dda49bbd6f4faadf2b84f758884c38
Ivo Dutra <ivogdutra@gmail.com>
2020-08-08 13:06:30 -0300

    add collectionView

--- ChuckApp/ChuckApp/Helpers/UI/ViewCodable.swift
+++ ChuckApp/ChuckApp/Helpers/UI/ViewCodable.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2020 Ivo Dutra. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ViewCodable {
+    /// Use to setup delegates and things like delegates and Images from imageViews
+    func configure()
+    /// Use to add any UI components inside the view
+    func setupHierarchy()
+    /// Setup constraints to the view and the subviews
+    func setupConstraints()
+    /// Customize the UIComponents(fonts, colors)
+    func render()
+    /// Setup any acessibility to the view
+    func setupAcessibilityIdentifiers()
+    /// This method builds the view using all the other methods fom the protocol
+    func setupView()
+}
+
+extension ViewCodable {
+
+    // Sometimes will not be implemented
+    func setupAcessibilityIdentifiers() { }
+
+    func setupView() {
+        configure()
+        setupHierarchy()
+        setupConstraints()
+        render()
+        setupAcessibilityIdentifiers()
+    }
+
+}
--- ChuckApp/ChuckApp/Module/Categories/CategoriesView.swift
+++ ChuckApp/ChuckApp/Module/Categories/CategoriesView.swift
@@ -10,4 +10,107 @@ import UIKit
 
 class CategoriesView: UIView {
 
+    // MARK: - Properties
+
+    var collectionView: UICollectionView
+    var cellId: String
+
+    var viewModel: CategoriesViewModelProtocol?
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        cellId = "categoryCollection"
+
+        // Initialize CollectionView
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 8
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+
+        super.init(frame: frame)
+
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+    // MARK: - Collection View
+
+extension CategoriesView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    var numberOfSections: Int { return 1 }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // viewModel.count
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        // Cells are a square with side = Screen width / 4
+        let side = collectionView.bounds.width/4.0
+
+        return CGSize(width: side, height: side)
+    }
+
+}
+
+    // MARK: - ViewCodable
+
+extension CategoriesView: ViewCodable {
+
+    func configure() {
+        configureCollectionView()
+    }
+
+    func setupHierarchy() {
+        self.addSubview(collectionView)
+    }
+
+    func setupConstraints() {
+        setCollectionViewContraints()
+    }
+
+    func render() {
+
+
+    }
+
+    // MARK: - ViewCodable Helpers
+
+    func configureCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.isPagingEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+
+    func setCollectionViewContraints() {
+
+        collectionView.setConstraints { (view) in
+                   view.topAnchor.constraint(equalTo: self.topAnchor, constant: 24).isActive = true
+                   view.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+                   // Apple Default tableView Spacing
+                   view.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+                   view.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20).isActive = true
+               }
+    }
+
 }
